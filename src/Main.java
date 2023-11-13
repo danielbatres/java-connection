3df71c651683ae204b2dfba4365be56ac01c48f9
danielbatres <danielbatresuser@gmail.com>
2023-11-13 12:24:40 -0600

    Update data

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,25 @@
+import javax.xml.transform.Result;
 import java.sql.*;
 import java.sql.DriverManager;
 
 public class Main {
     public static void main(String[] args) {
         Connection myConnection = null;
-        PreparedStatement myStatement = null;
+        Statement myStatement = null;
+        ResultSet myResult = null;
 
         try {
             myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "toor");
             System.out.println("Connected");
 
-            String sql = "INSERT INTO employees (first_name, pa_surname) VALUES (?, ?)";
+            myStatement = myConnection.createStatement();
 
-            myStatement = myConnection.prepareStatement(sql);
-            myStatement.setString(1, "Johana");
-            myStatement.setString(2, "Dorantes");
+            int rowsAffected = myStatement.executeUpdate("UPDATE employees SET email = 'johanador2@example.com' WHERE first_name = 'Johana'");
 
-            int rowsAffected = myStatement.executeUpdate();
+            myResult = myStatement.executeQuery("SELECT * FROM employees ORDER BY pa_surname");
 
-            if (rowsAffected > 0) {
-                System.out.println("new employee created");
+            while (myResult.next()) {
+                System.out.println(myResult.getString("first_name") + " " + myResult.getString("email"));
             }
         } catch (Exception e) {
             e.printStackTrace();
