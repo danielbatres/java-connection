ce8fee407dae5f6aa56cd1c6daea1e3795c199a0
danielbatres <danielbatresuser@gmail.com>
2023-11-13 12:52:43 -0600

    Project restructuring with IntelliJ and Maven

---
+++ crud-project/src/main/java/org/example/platzi/main/Main.java
@@ -0,0 +1,25 @@
+package org.example.platzi.main;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection myConnection = null;
+        Statement myStatement = null;
+        ResultSet myResult = null;
+
+        try {
+            myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "toor");
+            System.out.println("Connected");
+
+            myStatement = myConnection.createStatement();
+            myResult = myStatement.executeQuery("SELECT * FROM employees");
+
+            while (myResult.next()) {
+                System.out.println(myResult.getString("first_name"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
